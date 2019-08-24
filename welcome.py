51fdf6ed9ca52585aa2ac4b71188c0ebce975410
alialatiq <54315022+alialatiq@users.noreply.github.com>
2019-08-24 04:37:49 +0300

    test

--- welcome.py
+++ welcome.py
@@ -1,5 +1,26 @@
+#day1
 print("100 days of the code")
 
 print("Ali Abdullah Alatiq")
 
 print("Saudi Arabia - Najran")
+
+#day2
+print("Ali Abdullah Alatiq") #comment
+#print("Ali Abdullah Alatiq")
+# المبادرة السعوديةللمطورين
+#100daysofcode
+"""
+Ali Abdullah Alatiq
+ المبادرة السعوديةللمطورين
+100daysofcode
+"""
+#day3
+x = 8
+y = "Ali"
+print(x)
+print(y)
+
+f = 4
+f = "Ali Alatiq"
+print(f)
